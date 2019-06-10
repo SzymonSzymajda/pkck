9a7b20a217c867f3de5763ff245c16d964ac6cf0
Szymon <szymonicho@gmail.com>
2019-06-10 19:48:49 +0200

    serialization

---
+++ Zad5/Zad5/Serialization.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Zad5
+{
+    public static class Serialization
+    {
+        public static Tablica_zadań DeserializeFile(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Tablica_zadań));
+
+            using (Stream reader = new FileStream(filename, FileMode.Open))
+            {
+                return (Tablica_zadań)serializer.Deserialize(reader);
+            }
+        }
+
+        public static void SerializeToFile(string filename, Tablica_zadań root)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Tablica_zadań));
+            TextWriter writer = new StreamWriter(filename);
+
+            serializer.Serialize(writer, root);
+            writer.Close();
+        }
+    }
+}
--- Zad5/Zad5/XmlModel.cs
+++ Zad5/Zad5/XmlModel.cs
@@ -1,10 +1,4 @@
-﻿/* 
- Licensed under the Apache License, Version 2.0
-
- http://www.apache.org/licenses/LICENSE-2.0
- */
-using System;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 using System.Collections.Generic;
 namespace Zad5
 {
