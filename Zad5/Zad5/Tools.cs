8af169b676d7e6cb47f45a572b493e0a0d615010
Szymon <szymonicho@gmail.com>
2019-06-10 19:55:45 +0200

    added empty Tools

---
+++ Zad5/Zad5/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad5
+{
+    class Tools
+    {
+        public static void AddNewTask(Tablica_zadań root)
+        {
+
+        }
+
+        public static void AddNewEmployee(Tablica_zadań root)
+        {
+
+        }
+    }
+}
